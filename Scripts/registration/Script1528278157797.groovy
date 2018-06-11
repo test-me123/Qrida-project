beaca4fdb4a2fdeae0f05a4ed6db18e2d424dff7
SaiRahul <sairahulyadav3@gmail.com>
2018-06-11 18:16:56 +0530

    first 1

--- Scripts/registration/Script1528278157797.groovy
+++ Scripts/registration/Script1528278157797.groovy
@@ -21,3 +21,15 @@ import internal.GlobalVariable as GlobalVariable
 
 CustomKeywords.'com.login.qrida.loginQrida.openBrowser'()
 CustomKeywords.'com.registration.qrida.registrationLink.registration'(givenNameReg, surNameReg, emailReg, passwordReg, confirmReg)
+
+CustomKeywords.'com.login.qrida.loginQrida.openBrowser'()
+CustomKeywords.'com.registration.qrida.registrationLink.registration'(givenNameReg, surNameReg, emailReg, passwordReg, confirmReg)
+
+CustomKeywords.'com.login.qrida.loginQrida.openBrowser'()
+CustomKeywords.'com.registration.qrida.registrationLink.registration'(givenNameReg, surNameReg, emailReg, passwordReg, confirmReg)
+
+CustomKeywords.'com.login.qrida.loginQrida.openBrowser'()
+CustomKeywords.'com.registration.qrida.registrationLink.registration'(givenNameReg, surNameReg, emailReg, passwordReg, confirmReg)
+
+CustomKeywords.'com.login.qrida.loginQrida.openBrowser'()
+CustomKeywords.'com.registration.qrida.registrationLink.registration'(givenNameReg, surNameReg, emailReg, passwordReg, confirmReg)
